12fd931bb9b796e0262a798bcc429f3146d42ccd
SwarnimaKhodwe <51781759+SwarnimaKhodwe@users.noreply.github.com>
2019-06-28 14:36:15 +0530

    Update vgg_keras.py

--- vgg_keras.py
+++ vgg_keras.py
@@ -11,15 +11,9 @@ rk = vggnet_2()
 rk.read('car.jpg')
 
 x = rk.preprocess()
-print("SHAPE rk:  ",x.shape)
-print(type(x))
-print(x)
+
 preds = model.predict(x)
-#print("raw predicts:",preds)
-# decode the results into a list of tuples (class, description, probability)
-# (one such list for each sample in the batch)
-#print("type:",type(preds))
-#print("raw predicts:",np.argmax(preds))
+
 print('Predicted:', decode_predictions(preds, top=3)[0])
 # Predicted: [(u'n02504013', u'Indian_elephant', 0.82658225), (u'n01871265', u'tusker', 0.1122357), (u'n02504458', u'African_elephant', 0.061040461)]
 
